448a0f82b8ec08d2430674b1bd490fcc263ce9fc
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-11-24 10:22:56 -0600

    update to action fetch requests

--- src/actions/musicActions.js
+++ src/actions/musicActions.js
@@ -1,31 +1,34 @@
-import { NEW_MUSIC, FETCH_MUSICS, UPDATE_MUSIC, REMOVE_MUSIC } from './types';
+import { NEW_MUSIC, FETCH_MUSICS, REMOVE_MUSIC, UPDATE_MUSIC } from './types';
 
-export const newMusic = musicData => {
+export const newMusic = musicData => dispatch => {
     console.log(dispatch)
     fetch('http://localhost:3001/musics', {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json'},
+        headers: {
+            'Content-Type': 'application/json'
+        },
         body: JSON.stringify(musicData)
     })
     .then(response => response.json())
-    .then(music => {
+    .then(singular => {
+        console.log(singular)
         return dispatch({
             type: NEW_MUSIC,
-            payload: music
+            payload: singular
         })
-    });
+    }
+    );
 };
+
 
 export const fetchMusics = (id) => dispatch => {
     fetch('http://localhost:3001/musics')
     .then(response => response.json())
-    .then(music => dispatch({
-        type: FETCH_MUSICS,
-        payload: music
-    }))
+    .then(plural => dispatch({ type: FETCH_MUSICS, payload: plural }))
 }
 
 export const deleteMusic = id => dispatch => {
+    console.log(dispatch)
     fetch(`http://localhost:3001/musics/${id}`, {
         method: 'DELETE',
         headers: {
@@ -34,27 +37,32 @@ export const deleteMusic = id => dispatch => {
         body: JSON.stringify({id})
     })
     .then(response => response.json())
-    .then(music => {
+    .then(singular => {
+        console.log(singular)
         return dispatch({
             type: REMOVE_MUSIC,
-            payload: music
+            payload: singular
         })
-    });
+    }
+    );
 };
 
 export const updateMusic = id => dispatch => {
+    console.log(dispatch)
     fetch(`http://localhost:3001/musics/${id}`, {
-        method: "PATCH",
+        method: 'PATCH',
         headers: {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({id})
     })
-    .then?(response => response.json())
-    .then(music => {
+    .then(response => response.json())
+    .then(singular => {
+        console.log(singular)
         return dispatch({
             type: UPDATE_MUSIC,
-            payload: music
+            payload: singular
         })
-    })
-}
+    }
+    );
+};
--- src/reducers/musicReducer.js
+++ src/reducers/musicReducer.js
@@ -1,13 +1,22 @@
-import { NEW_MUSIC, FETCH_MUSICS, UPDATE_MUSIC, REMOVE_MUSIC } from '../actions/types';
+import { NEW_MUSIC, FETCH_MUSICS, REMOVE_MUSIC } from '../actions/types';
+// UPDATE_MUSIC
+const initialState = { group: [], };
 
-const initialState = { group: [] };
-
-const musicReducer = ( state = initialState, action ) => {
+function musicReducer(state = initialState, action) {
     switch(action.type) {
         case NEW_MUSIC:
             let list = state.group;
             list.push(action.payload);
             return { ...state, group: list }
+        case FETCH_MUSICS:
+            let listofmusic = action.payload;
+            return { ...state, group: listofmusic };
+        // case UPDATE_MUSIC:
+            // filter out old
+        case REMOVE_MUSIC:
+            let filteredlist = state.group.filter( tune => {
+                return tune.id !== action.payload.id })
+            return { ...state, group: filteredlist }
         default:
             return state;
     }
