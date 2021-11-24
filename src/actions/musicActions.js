125dc08aa8e2cbd1a702ccfba64453aee1271c7f
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-11-24 10:09:16 -0600

    work on the action fetch types

--- src/actions/musicActions.js
+++ src/actions/musicActions.js
@@ -1,4 +1,4 @@
-import { NEW_MUSIC } from './types';
+import { NEW_MUSIC, FETCH_MUSICS, UPDATE_MUSIC, REMOVE_MUSIC } from './types';
 
 export const newMusic = musicData => {
     console.log(dispatch)
@@ -15,3 +15,46 @@ export const newMusic = musicData => {
         })
     });
 };
+
+export const fetchMusics = (id) => dispatch => {
+    fetch('http://localhost:3001/musics')
+    .then(response => response.json())
+    .then(music => dispatch({
+        type: FETCH_MUSICS,
+        payload: music
+    }))
+}
+
+export const deleteMusic = id => dispatch => {
+    fetch(`http://localhost:3001/musics/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({id})
+    })
+    .then(response => response.json())
+    .then(music => {
+        return dispatch({
+            type: REMOVE_MUSIC,
+            payload: music
+        })
+    });
+};
+
+export const updateMusic = id => dispatch => {
+    fetch(`http://localhost:3001/musics/${id}`, {
+        method: "PATCH",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({id})
+    })
+    .then?(response => response.json())
+    .then(music => {
+        return dispatch({
+            type: UPDATE_MUSIC,
+            payload: music
+        })
+    })
+}
--- src/reducers/musicReducer.js
+++ src/reducers/musicReducer.js
@@ -1,11 +1,11 @@
-import { NEW_MUSIC } from '../actions/types';
+import { NEW_MUSIC, FETCH_MUSICS, UPDATE_MUSIC, REMOVE_MUSIC } from '../actions/types';
 
 const initialState = { group: [] };
 
 const musicReducer = ( state = initialState, action ) => {
     switch(action.type) {
         case NEW_MUSIC:
-            let list = state.bands;
+            let list = state.group;
             list.push(action.payload);
             return { ...state, group: list }
         default:
